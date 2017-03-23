d648899048d16d8cf40ef9aa31d5ae95df41e1a9
Micknick9 <19KMusgrove@lcamn.org>
2017-03-22 23:00:36 -0500

    Finished Craps and added a wins/loses counter.

--- crapsGame.py
+++ crapsGame.py
@@ -5,7 +5,7 @@ print("""
 | |/\| |/ _ \ |/ __/ _ \| '_ ` _ \ / _ \ | |
 \  /\  /  __/ | (_| (_) | | | | | |  __/ |_|
  \/  \/ \___|_|\___\___/|_| |_| |_|\___| (_)
- 
+
 """)
 
 print ("welcome to Craps!")
@@ -16,11 +16,13 @@ import random
 
 play = "y"
 played = "n"
+wins = 0
+loses = 0
 
 play = input("Would you Like to play?(y/n) ")
 if play == "y":
     print("Sweet! let's get started.")
-    while play == "y"
+    while play == "y":
         if played == "n":
             die1 = random.randrange(6)+1
             die2 = random.randrange(6)+1
@@ -28,8 +30,35 @@ if play == "y":
             print("You rolled a ", die1, "and a ", die2, "for a total of ", total)
             played = "y"
         else:
-            if total == 7 or total == 11:
-                
+            while play == "y":
+                if total == 7 or total == 11:
+                    play = input("Congrats! Would you like to play again?(y/n) ")
+                    print("")
+                    wins = wins + 1
+                    if play == "y":
+                        die1 = random.randrange(6)+1
+                        die2 = random.randrange(6)+1
+                        total = die1 + die2
+                        print("You rolled a ", die1, "and a ", die2, "for a total of ", total)
+                    elif play == "n":
+                        print("You won a total of ", wins, "times and lost a total of ", loses, "times.")
+                        print("Ok, thanks for playing!")
+                    else:
+                        print("please only enter the letter 'y'to indicate yes, or the letter 'n' to indicate no.")
+                else:
+                    play = input("Darn! Would you like to try again?(y/n) ")
+                    print("")
+                    loses = loses +1
+                    if play == "y":
+                        die1 = random.randrange(6)+1
+                        die2 = random.randrange(6)+1
+                        total = die1 + die2
+                        print("You rolled a ", die1, "and a ", die2, "for a total of ", total)
+                    elif play == "n":
+                        print("You won a total of ", wins, "times and lost a total of ", loses, "times.")
+                        print("Ok, thanks for playing!")
+                    else:
+                        print("please only enter the letter 'y'to indicate yes, or the letter 'n' to indicate no.")
 elif play == "n":
     print("OK, have a good day!")
 else:
